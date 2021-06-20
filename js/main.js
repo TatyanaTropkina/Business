9538f5de65f4c9bf86b86930f93b9244d4b4a784
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-06-21 00:46:34 +0300

    change main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,25 @@
 $(function () {
-
     $('.menu__btn').on('click', function () {
-        $('.menu__btn').toggleClass('menu__btn--active');
-        $('.menu__list').toggleClass('menu__list--active');
-
+        $('.menu__btn').toggleClass('active');
+        $('.menu__list').toggleClass('active');
+		$('.header__bg').toggleClass('active');
     });
     $('.menu__list-link').on('click', function () {
-        $('.menu__btn').removeClass('menu__btn--active');
-        $('.menu__list').removeClass('menu__list--active');
-
+        $('.menu__btn').removeClass('active');
+        $('.menu__list').removeClass('active');
     });
-
     $(window).scroll(function () {
-        if (window.matchMedia("(max-width: 750px)").matches) {
-            $('.menu__btn').removeClass('menu__btn--active');
-            $(".menu__list").removeClass("menu__list--active");
-
+        if (window.matchMedia("(max-width: 769px)").matches) {
+            $('.menu__btn').removeClass('active');
+            $(".menu__list").removeClass("active");
         }
     });
-
     $('.invest__slider').slick({
         prevArrow: '<button class="slider-btn slider-btn__left"><img src="images/arrow-prev.svg" alt=""></button>',
         nextArrow: '<button class="slider-btn slider-btn__right"><img src="images/arrow-next.svg" alt=""></button>',
         infinite: true,
-        focusOnSelect: false,
         speed: 2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-
-    });
-
-    $('.invest__slider').on('click', function () {
-        $(this).slick('slickNext');
+		autoplay: true,
     });
     $('.traffic__slider').slick({
         arrows: false,
@@ -39,19 +27,32 @@ $(function () {
         autoplay: true,
         infinite: true,
         fade: true,
-        autoplaySpeed: 3000,
-        pauseOnHover: true,
-        swipe: true,
+        autoplaySpeed: 3000, 
     });
-    $('.traffic__slider').on('click', function () {
-        $(this).slick('slickNext');
-    });
-
+let trafficSlider = $('.traffic__slider');
+$(window).scroll(function(){
+	if ($(this).scrollTop()> trafficSlider.offset().top - 425) {
+	trafficSlider.addClass('anim');
+ }
 });
-
-wow = new WOW({
-
-    mobile: false,
-
-})
+	let invest = $('.invest')
+$(window).scroll(function(){
+	if($(this).scrollTop()> invest.offset().top - 200) {
+		invest.addClass('anim');
+	}
+});
+	let featureDots = $('.feature__img-wrapper');
+	let featureInner = $('.feature__inner');
+	let feature = $('.feature');
+$(window).scroll(function () {
+	if ($(this).scrollTop() > feature.offset().top - 300) {
+		featureInner.addClass('anim');
+		featureDots.addClass('anim');
+	}
+});
+});
+wow = new  WOW({
+	mobile: false})
 wow.init();
+
+
